a2516334abfa257ff4f926e3c827fef51375109a
TiM-SyStEm <r44t.rmt@yandex.ru>
2026-09-22 12:16:59 +0300

    Update arrays

--- src/lab02/README.md
+++ src/lab02/README.md
@@ -23,7 +23,7 @@ def min_max(nums: list[float | int]) -> tuple[float | int, float | int]:
 
     if len(nums) == 0:
         raise ValueError("List is empty.")
-    return (min(nums), max(nums))
+    return min(nums), max(nums)
 
 # Test-cases
 print(min_max([3, -1, 5, 5, 0]))
--- src/lab02/arrays.py
+++ src/lab02/arrays.py
@@ -18,7 +18,7 @@ def min_max(nums: list[float | int]) -> tuple[float | int, float | int]:
 
     if len(nums) == 0:
         raise ValueError("List is empty.")
-    return (min(nums), max(nums))
+    return min(nums), max(nums)
 
 
 def unique_sorted(nums: list[float | int]) -> list[float | int]:
